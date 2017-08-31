5f95f8c1da3abb70b00ee5d1d3c9a7efbdef9962
hknam <hknam0205@gmail.com>
2017-08-31 14:35:07 +0900

    config keyword add

--- mitmproxy/make_config.py
+++ mitmproxy/make_config.py
@@ -10,7 +10,7 @@ config['filename']['controller'] = 'controller.log'
 config['filename']['browser'] = 'browser.log'
 config['filename']['reader'] = 'reader.log'
 config['filename']['finder'] = 'finder.log'
-
+config['filename']['keyword'] = 'iphone'
 
 config['proxy'] = {}
 
@@ -26,7 +26,7 @@ config['webdriver'] = {}
 config['webdriver']['path'] = './geckodriver'
 config['webdriver']['base_url'] = 'about:blank'
 
-config['keyword'] = 'iphone'
+
 
 with open('config.ini', 'w') as configfile:
     config.write(configfile)
